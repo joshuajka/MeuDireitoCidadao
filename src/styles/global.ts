38f7f0d2547d5e119bb1d7622668c30693e50626
Gabriel <gabriel_silva@discente.ufg.br>
2020-10-31 09:12:36 -0300

    Add Styled Components

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+
+  body, html, #root {
+     height: 100%;
+  }
+
+  body {
+     background-color: white;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
